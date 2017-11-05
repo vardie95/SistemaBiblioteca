7b48f78cf51161946cff69f23d881b96a44012e3
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2017-11-05 13:30:14 -0600

    Estadistica genero y cantidad listos

--- SistemaBiblioteca/Estadisticas.Designer.cs
+++ SistemaBiblioteca/Estadisticas.Designer.cs
@@ -34,18 +34,21 @@
             this.button5 = new System.Windows.Forms.Button();
             this.cbEstadistica = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCalcSelecc = new System.Windows.Forms.Button();
+            this.btnCalcTodas = new System.Windows.Forms.Button();
+            this.dgEstadistica = new System.Windows.Forms.DataGridView();
+            this.estadistica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resultado = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.fechaEstadistica = new System.Windows.Forms.DateTimePicker();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEstadistica)).BeginInit();
             this.SuspendLayout();
             // 
             // cbTipoPrestamo
             // 
             this.cbTipoPrestamo.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cbTipoPrestamo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbTipoPrestamo.FormattingEnabled = true;
             this.cbTipoPrestamo.Items.AddRange(new object[] {
             "Computadora",
@@ -81,6 +84,7 @@
             // cbEstadistica
             // 
             this.cbEstadistica.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.cbEstadistica.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbEstadistica.FormattingEnabled = true;
             this.cbEstadistica.Items.AddRange(new object[] {
             "Cantidad",
@@ -96,40 +100,62 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(11, 37);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.Size = new System.Drawing.Size(81, 13);
             this.label2.TabIndex = 35;
-            this.label2.Text = "Estadistica por:";
+            this.label2.Text = "Estadística por:";
             // 
-            // button1
+            // btnCalcSelecc
             // 
-            this.button1.BackColor = System.Drawing.Color.SteelBlue;
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(14, 74);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 23);
-            this.button1.TabIndex = 37;
-            this.button1.Text = "Calcular selección";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnCalcSelecc.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCalcSelecc.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcSelecc.Location = new System.Drawing.Point(14, 74);
+            this.btnCalcSelecc.Name = "btnCalcSelecc";
+            this.btnCalcSelecc.Size = new System.Drawing.Size(105, 23);
+            this.btnCalcSelecc.TabIndex = 37;
+            this.btnCalcSelecc.Text = "Calcular selección";
+            this.btnCalcSelecc.UseVisualStyleBackColor = false;
+            this.btnCalcSelecc.Click += new System.EventHandler(this.btnCalcSelecc_Click);
             // 
-            // button2
+            // btnCalcTodas
             // 
-            this.button2.BackColor = System.Drawing.Color.SteelBlue;
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(127, 74);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(105, 23);
-            this.button2.TabIndex = 38;
-            this.button2.Text = "Calcular todas";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.btnCalcTodas.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCalcTodas.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcTodas.Location = new System.Drawing.Point(127, 74);
+            this.btnCalcTodas.Name = "btnCalcTodas";
+            this.btnCalcTodas.Size = new System.Drawing.Size(105, 23);
+            this.btnCalcTodas.TabIndex = 38;
+            this.btnCalcTodas.Text = "Calcular todas";
+            this.btnCalcTodas.UseVisualStyleBackColor = false;
+            this.btnCalcTodas.Click += new System.EventHandler(this.btnCalcTodas_Click);
             // 
-            // dataGridView1
+            // dgEstadistica
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(14, 103);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(429, 254);
-            this.dataGridView1.TabIndex = 39;
+            this.dgEstadistica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEstadistica.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.estadistica,
+            this.Fecha,
+            this.Resultado});
+            this.dgEstadistica.Location = new System.Drawing.Point(14, 103);
+            this.dgEstadistica.Name = "dgEstadistica";
+            this.dgEstadistica.Size = new System.Drawing.Size(429, 254);
+            this.dgEstadistica.TabIndex = 39;
+            // 
+            // estadistica
+            // 
+            this.estadistica.HeaderText = "Estadística";
+            this.estadistica.Name = "estadistica";
+            this.estadistica.Width = 150;
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "fecha";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Resultado
+            // 
+            this.Resultado.HeaderText = "resultado";
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Width = 130;
             // 
             // fechaEstadistica
             // 
@@ -139,27 +165,17 @@
             this.fechaEstadistica.TabIndex = 41;
             this.fechaEstadistica.ValueChanged += new System.EventHandler(this.fechaEstadistica_ValueChanged);
             // 
-            // button3
+            // btnLimpiar
             // 
-            this.button3.BackColor = System.Drawing.Color.Green;
-            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(310, 74);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(133, 23);
-            this.button3.TabIndex = 42;
-            this.button3.Text = "Guardar para comparar";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // button4
-            // 
-            this.button4.BackColor = System.Drawing.Color.Brown;
-            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button4.Location = new System.Drawing.Point(238, 74);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(66, 23);
-            this.button4.TabIndex = 43;
-            this.button4.Text = "Limpiar";
-            this.button4.UseVisualStyleBackColor = false;
+            this.btnLimpiar.BackColor = System.Drawing.Color.Brown;
+            this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnLimpiar.Location = new System.Drawing.Point(238, 74);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(66, 23);
+            this.btnLimpiar.TabIndex = 43;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
             // 
             // Estadisticas
             // 
@@ -167,12 +183,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(457, 369);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnLimpiar);
             this.Controls.Add(this.fechaEstadistica);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgEstadistica);
+            this.Controls.Add(this.btnCalcTodas);
+            this.Controls.Add(this.btnCalcSelecc);
             this.Controls.Add(this.cbEstadistica);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.cbTipoPrestamo);
@@ -182,7 +197,7 @@
             this.Name = "Estadisticas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Estadísticas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEstadistica)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,11 +210,13 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.ComboBox cbEstadistica;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCalcSelecc;
+        private System.Windows.Forms.Button btnCalcTodas;
+        private System.Windows.Forms.DataGridView dgEstadistica;
         private System.Windows.Forms.DateTimePicker fechaEstadistica;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.DataGridViewTextBoxColumn estadistica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resultado;
     }
 }
--- SistemaBiblioteca/Estadisticas.cs
+++ SistemaBiblioteca/Estadisticas.cs
@@ -12,10 +12,16 @@ namespace SistemaBiblioteca
 {
     public partial class Estadisticas : Form
     {
+
+        Conexion con;
         public Estadisticas()
         {
+            con = new Conexion(@"Data Source = localhost;port=3306;Initial"
+            + " Catalog=sistemabiblioteca;User Id=root;password = '' ");
+
             InitializeComponent();
             SetMyCustomFormat();
+
         }
 
         private void cbTipoPrestamo_SelectedIndexChanged(object sender, EventArgs e)
@@ -40,11 +46,6 @@ namespace SistemaBiblioteca
             fechaEstadistica.CustomFormat = "MM/yyyy";
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button5_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -55,5 +56,145 @@ namespace SistemaBiblioteca
         {
 
         }
+
+        private void btnCalcSelecc_Click(object sender, EventArgs e)
+        {
+            if(cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Computadora")
+            {
+                calcularComputadora(cbEstadistica.GetItemText(cbEstadistica.SelectedItem));
+            }
+            if (cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Cubículo")
+            {
+                calcularCubiculo(cbEstadistica.GetItemText(cbEstadistica.SelectedItem));
+            }
+            if (cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Libro")
+            {
+                calcularLibro(cbEstadistica.GetItemText(cbEstadistica.SelectedItem));
+            }
+        }
+
+        private void calcularComputadora(string estadistica)
+        {
+            if(estadistica == "Cantidad" || estadistica == "")
+            {
+                calcularCantidadPrestamos("Computdora","computadoras","fecha");
+            }
+            if (estadistica == "Género" || estadistica == "")
+            {
+                calcularCantidadSexo("Computdora", "computadoras", "fecha");
+            }
+        }
+
+        private void calcularCubiculo(string estadistica)
+        {
+            if (estadistica == "Cantidad" || estadistica == "")
+            {
+                calcularCantidadPrestamos("Cubículo", "cubiculo","fecha");
+            }
+            if (estadistica == "Género" || estadistica == "")
+            {
+                calcularCantidadSexo("Cubículo", "cubiculo", "fecha");
+            }
+        }
+
+
+        private void calcularLibro(string estadistica)
+        {
+            if (estadistica == "Cantidad" || estadistica == "")
+            {
+                calcularCantidadPrestamos("Libro","libro", "fechaPrestamo");
+            }
+            if (estadistica == "Género" || estadistica == "")
+            {
+                calcularCantidadSexo("Libro", "libro", "fechaPrestamo");
+            }
+        }
+       
+
+        private void calcularCantidadPrestamos(string nombre,string tabla,string fecha)
+        {
+            try
+            {
+                con.Abrir();
+                con.CargarQuery("SELECT count(*) FROM " + tabla + " WHERE "+ fecha + " like '%/"+ 
+                    fechaEstadistica.Text+ "';");
+                
+                IDataReader reader = con.GetSalida();
+                DataGridViewRow row = (DataGridViewRow)dgEstadistica.Rows[0].Clone();
+
+                while (reader.Read())
+                {
+                    row.Cells[0].Value = ""+nombre+"/Cantidad";
+                    row.Cells[1].Value = fechaEstadistica.Text;
+                    row.Cells[2].Value = reader["count(*)"].ToString();
+                    dgEstadistica.Rows.Add(row);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "Error");
+
+            }
+            finally
+            {
+                con.Cerrar();
+            }
+        }
+
+        private void calcularCantidadSexo(string nombre, string tabla, string fecha)
+        {
+            try
+            {
+                con.Abrir();
+                string fechaBusqueda = tabla + "." + fecha;
+                con.CargarQuery("SELECT count(*),cliente.Sexo FROM " + tabla + ",cliente" +
+                    " WHERE " + fechaBusqueda + " like '%/"+fechaEstadistica.Text + "' AND cliente.cedula = "+tabla+".cedula" +
+                    " GROUP BY cliente.Sexo; ");
+                IDataReader reader = con.GetSalida();
+                string resultado="";
+
+                while (reader.Read())
+                {
+                    resultado += reader["Sexo"].ToString()+ ":" +reader["count(*)"].ToString()+ " ";
+                }
+                DataGridViewRow row = (DataGridViewRow)dgEstadistica.Rows[0].Clone();
+                row.Cells[0].Value = "" + nombre + "/Género";
+                row.Cells[1].Value = fechaEstadistica.Text;
+                row.Cells[2].Value = resultado;
+                dgEstadistica.Rows.Add(row);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex, "Error");
+
+            }
+            finally
+            {
+                con.Cerrar();
+            }
+        }
+
+
+        private void btnCalcTodas_Click(object sender, EventArgs e)
+        {
+            if (cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Computadora")
+            {
+                calcularComputadora("");
+            }
+            if (cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Cubículo")
+            {
+                calcularCubiculo("");
+            }
+            if (cbTipoPrestamo.GetItemText(cbTipoPrestamo.SelectedItem) == "Libro")
+            {
+                calcularLibro("");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            dgEstadistica.Rows.Clear();
+        }
+
     }
 }
